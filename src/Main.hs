3f33c0d404a2b6732fd2e89c0b0e024a22826fb1
myuon <ioi.joi.koi.loi@gmail.com>
2017-12-09 20:31:05 +0900

    fetching sentinel

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Brick.BChan
 import qualified Brick.Widgets.List as W
 import qualified Graphics.Vty as Vty
 import Control.Lens
+import Control.Monad
 import Control.Concurrent (forkIO, threadDelay)
 import qualified Data.Vector as V
 import qualified Data.Text as T
@@ -20,13 +21,14 @@ data Card
 makeLenses ''Card
 
 renderCard :: Bool -> Card -> Widget n
-renderCard _ card =
-  txt "[" <+> txt (card ^. plugin) <+> txt "] " <+> txt (card ^. title)
+renderCard selected card =
+  ((if selected then withAttr "inverted" else id) $ txt "[" <+> txt (card ^. plugin) <+> txt "] " <+> txt (card ^. title))
   <=> txt (card ^. content)
 
 data Client
   = Client
-  { _timeline :: W.List String Card
+  { _size :: (Int,Int)
+  , _timeline :: W.List String Card
   }
 
 makeLenses ''Client
@@ -37,28 +39,40 @@ app = App
   (\_ _ -> Nothing)
   evhandler
   return
-  (\_ -> attrMap (fg Vty.white) [])
+  (\_ -> attrMap (fg Vty.white) colorscheme)
 
   where
-    renderer cli =
+    renderer cli = return $ vBox
       [ W.renderList renderCard True (cli ^. timeline)
+      , withAttr "inverted" $ padRight Max $ txt "--- *timeline* [tw/tm]"
       ]
 
     evhandler cli = \case
       VtyEvent (Vty.EvKey (Vty.KChar 'q') []) -> halt cli
-      AppEvent card -> continue $ cli & timeline %~ W.listInsert (cli ^. timeline ^. W.listElementsL ^. to length) card
+      VtyEvent evkey -> handleEventLensed cli timeline W.handleListEvent evkey >>= continue
+      AppEvent card -> do
+        continue $ cli &~ do
+          timeline %= W.listInsert (cli ^. timeline ^. W.listElementsL ^. to length - 1) card
+
+          let Just (_,sel) = cli ^. timeline ^. to W.listSelectedElement
+          when (sel ^. title == "sentinel") $ timeline %= W.listMoveDown
       _ -> continue cli
+
+    colorscheme =
+      [ ("inverted", Vty.black `on` Vty.white)
+      ]
 
 fetch :: BChan Card -> IO ()
 fetch chan = do
   threadDelay 1000000
   time <- getCurrentTime
-  writeBChan chan $ Card "tw" "timer" (T.pack $ show time)
+  writeBChan chan $ Card "tm" "timer" (T.pack $ show time)
   fetch chan
 
 main :: IO ()
 main = do
-  let cli = Client (W.list "timeline" (V.fromList [Card "tw" "みょん @myuon_myon" "てすと"]) 2)
+  size <- Vty.displayBounds =<< Vty.outputForConfig =<< Vty.standardIOConfig
+  let cli = Client size (W.list "timeline" (V.fromList [Card "tw" "みょん @myuon_myon" "てすと", Card "sys" "sentinel" "--- fetching new cards ---"]) 2)
   chan <- newBChan 2
 
   forkIO $ fetch chan
